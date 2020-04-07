676b819d34f2158ba8292f341b6db58742761704
zyqifly <zyqifly@hotmail.com>
2020-04-07 09:48:49 +0800

    管道，测试成功，两个进程间可通信。

---
+++ FIFO/FIFO_CLIENT/FIFO_CLIENT.cpp
@@ -0,0 +1,33 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "FIFO_CLIENT.h"
+
+using namespace std;
+#include<iostream>
+#include<windows.h>
+#include<ctime>
+using namespace std;
+
+
+int main()
+{
+    HANDLE hPipe = CreateNamedPipe(TEXT("\\\\.\\Pipe\\mypipe"), PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT
+        , PIPE_UNLIMITED_INSTANCES, 0, 0, NMPWAIT_WAIT_FOREVER, 0);//创建了一个命名管道
+    if (ConnectNamedPipe(hPipe, NULL) == TRUE)//
+    {
+        cout << "Connected!" << endl;
+    }
+
+    char buf[256] = "Hello Pipe";
+    DWORD wlen = 0;
+
+    while (1)
+    {
+        WriteFile(hPipe, buf, sizeof(buf), &wlen, 0);
+        cout << "Send:" << buf << endl;
+        _sleep(1000);
+    }
+    return 1;
+}
+
---
+++ FIFO/FIFO_SERVER/FIFO_SERVER.cpp
@@ -0,0 +1,39 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "FIFO_SERVER.h"
+
+using namespace std;
+
+#include<iostream>
+#include<windows.h>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+    while (WaitNamedPipe(TEXT("\\\\.\\Pipe\\mypipe"), NMPWAIT_WAIT_FOREVER) == FALSE)
+    {
+
+    }
+
+    HANDLE hPipe = CreateFile(TEXT("\\\\.\\Pipe\\mypipe"), GENERIC_READ | GENERIC_WRITE, 0,
+        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    char buf[256];
+    DWORD rlen = 0;
+    while (1)
+    {
+        if (ReadFile(hPipe, buf, 256, &rlen, NULL) == FALSE)//读取管道中的内容（管道是一种特殊的文件）
+        {
+            CloseHandle(hPipe);//关闭管道
+            break;
+        }
+        else
+        {
+            cout << "Rcv:" << buf << endl;
+        }
+    }
+    return 0;
+}
+
+
