ad52e71f79939816f82a80452ec54af3e0f51a55
zyqifly <zyqifly@hotmail.com>
2020-04-16 19:50:38 +0800

    完成了拉伸机的集成，并编写了一个Client的DEMO。

--- FIFO/DICCLIENT/DICCLIENT.cpp
+++ FIFO/DICCLIENT/DICCLIENT.cpp
@@ -13,12 +13,12 @@ using namespace std;
 int main()
 {
     pipeClient* myClient = new   pipeClient();/* = new pipeClient(TEXT("\\\\.\\Pipe\\mypipe"));*/
-    //thread t(&pipeClient::connect, myClient);
-    myClient->connect();
+    //thread t(&pipeClient::connect, myClient);//此函数将连接的动作发送到另一个线程
+    myClient->connect();//
     while (true)
     {
         int a = 0;
-        cout << "输入1继续。。。" << endl;
+        cout << "输入数字继续，0退出、1自动运行、2停止、3读取状态。。。" << endl;
         cin >> a;
         if (a==0)
         {
@@ -36,24 +36,24 @@ int main()
             {
                 char    data[] = ":auto";
                 myClient->send(data, sizeof(data));
-                continue;
+                //continue;
             }
             else if (a==2)
             {
                 char    data[] = ":stop";
                 myClient->send(data, sizeof(data));
-                continue;
+                //continue;
             }
             else  if (a==3)
             {
                 char    data[] = ":req";
                 myClient->send(data, sizeof(data));
                 Sleep(1000);
-                //char    data1[256] = {};
-                //myClient->read(data1, sizeof(data1));
-                continue;
+                char    data1[256] = {};
+                myClient->read(data1, sizeof(data1));
+                //continue;
             }
-            else
+            /*else
             {
                 char    data[] = "hello";
                 myClient->send(data, sizeof(data));
@@ -61,7 +61,7 @@ int main()
                 char    data1[256] = {};
                 myClient->read(data1, sizeof(data1));
                 continue;
-            }
+            }*/
             
             
         }
